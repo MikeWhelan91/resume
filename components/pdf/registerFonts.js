41c5d9ea709a274eafdaf7cdeb722448c3b23c80
MikeWhelan91 <whelano07@hotmail.com>
2025-09-05 17:51:57 +0100

    font issue styff

--- components/pdf/registerFonts.js
+++ components/pdf/registerFonts.js
@@ -1,7 +1,7 @@
 import { Font } from "@react-pdf/renderer";
 
 // Default face (weight 400) so fontWeight:400 resolves
-Font.register({ family: "InterPDF", src: "/fonts/Inter-Regular.ttf" });
+Font.register({ family: "InterPDF", src: "/fonts/Inter-Regular.ttf", fontWeight: 400 });
 
 // Additional weights
 Font.register({ family: "InterPDF", src: "/fonts/Inter-Medium.ttf",   fontWeight: 500 });
