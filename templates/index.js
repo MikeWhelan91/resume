96e8953ce29c924226896e4af88b8539d785f007
Mike Whelan <43212748+MikeWhelan91@users.noreply.github.com>
2025-09-09 22:09:31 +0100

    feat: allow raw template imports

--- templates/index.js
+++ templates/index.js
@@ -1,9 +1,9 @@
 export { templates } from './registry.generated'
-export function getTemplate(id){
-  const all = require('./registry.generated.js').templates
-  return all.find(t => t.id === id) || all[0]
+
+export function getTemplate(id) {
+  return templates.find(t => t.id === id) || templates[0]
 }
-export function listTemplates(){
-  const all = require('./registry.generated.js').templates
-  return all.map(t => ({ id: t.id, name: t.name, engine: t.engine }))
+
+export function listTemplates() {
+  return templates.map(t => ({ id: t.id, name: t.name, engine: t.engine }))
 }
