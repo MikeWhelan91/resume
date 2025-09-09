95ab35babe9c86735f9b671e3b71876c0d0e67b7
Mike Whelan <43212748+MikeWhelan91@users.noreply.github.com>
2025-09-09 23:13:17 +0100

    Fix react alias resolution and add root path alias

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
+      'react$': require.resolve('react'),
+      'react-dom$': require.resolve('react-dom'),
+      '@': path.resolve(__dirname),
     };
     return config;
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
